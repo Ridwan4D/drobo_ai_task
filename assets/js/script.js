ee7059a14dedcad763c97505a1a198423197b633
Ridwan4D <ridwanul161@gmail.com>
2025-03-18 11:56:10 +0600

    navbar is responsive for small screen

---
+++ assets/js/script.js
@@ -0,0 +1,13 @@
+let menuList = document.getElementById('menu_list');
+
+menuList.style.maxHeight = "0px";
+
+function toggleMenu() {
+    if (menuList.style.maxHeight == "0px") {
+        menuList.style.maxHeight = "300px"
+        menuList.style.padding = "20px 0"
+    } else {
+        menuList.style.maxHeight = "0px"
+        menuList.style.padding = "0"
+    }
+}
