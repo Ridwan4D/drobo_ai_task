c0eb17ab34be327e2d617b53d1f6e0a09fcf176a
Ridwan4D <ridwanul161@gmail.com>
2025-03-18 13:03:16 +0600

    header section created

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,9 +1,9 @@
 let menuList = document.getElementById('menu_list');
 
-menuList.style.maxHeight = "0px";
+// menuList.style.maxHeight = "0px";
 
 function toggleMenu() {
-    if (menuList.style.maxHeight == "0px") {
+    if (menuList.style.maxHeight === "0px" || menuList.style.maxHeight === "") {
         menuList.style.maxHeight = "300px"
         menuList.style.padding = "20px 0"
     } else {
