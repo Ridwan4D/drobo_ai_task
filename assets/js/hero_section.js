d32c5e7539c563c3ad781a8dba1dc35247fadffe
Ridwan4D <ridwnaul161@gmail.com>
2025-03-26 15:01:38 +0600

    mega menu setup

--- assets/js/hero_section.js
+++ assets/js/hero_section.js
@@ -1,12 +1,10 @@
 const btn_id = document.getElementById("navbarDropdown");
 const mega_menu = document.querySelector(".mega_menu");
 
-btn_id.addEventListener("click", () => {
-    if (mega_menu.style.display === "block") {
-        mega_menu.style.display = "none";
-    } else {
-        mega_menu.style.display = "block";
-    }
+btn_id.addEventListener("mouseover", () => {
+    mega_menu.style.display = "block";
 });
 
-
+btn_id.addEventListener("click", () => {
+    mega_menu.style.display = "none";
+});
